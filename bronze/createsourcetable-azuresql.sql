84b657ccbb5e7569fd52e8b7bf19535b07be75d4
sachinbb7 <118004516+sachinbb7@users.noreply.github.com>
2025-09-15 12:22:01 +0530

    Add files via upload

---
+++ bronze/createsourcetable-azuresql.sql
@@ -0,0 +1,14 @@
+CREATE TABLE care_sales  (
+Branch_ID VARCHAR(200),
+Dealer_ID VARCHAR(200),
+Model_ID VARCHAR(200),
+Revenue BIGINT,
+Units_Sold BIGINT,
+Date_ID VARCHAR(200),
+Day INT,
+Month INT,
+Year INT,
+BranchName VARCHAR(200),
+DealerName VARCHAR(2000),
+Product_Name VARCHAR(2000)
+)
